35bd12940943ebde33d5dc2ddb9b0bdc96abd073
nablaOp <krll.ptrv@gmail.com>
2022-12-02 11:50:02 +0000

    day-2 part two

--- day-2/rps.exs
+++ day-2/rps.exs
@@ -7,23 +7,56 @@ defmodule RPS do
     fileName |> getInput |> Enum.reduce(0, fn play, acc -> acc + (play |> String.split(" ") |> getRoundResult) end)
   end
 
+  def secondGame(fileName) do
+    fileName |> getInput |> Enum.reduce(0, fn play, acc -> acc + (play |> String.split(" ") |> getRoundResultPartTwo |> IO.inspect) end)
+  end
+
   defguard is_draw(play) when play == {"A", "X"} or play == {"B", "Y"} or play == {"C", "Z"}
+  defguard is_draw_partTwo(r) when r == "Y"
 
   defguard is_leftWon(play) when play == {"A", "Z"} or play == {"B", "X"} or play == {"C", "Y"}
+  defguard is_leftWon_partTwo(r) when r == "X"
 
   def getRoundResult(play) do
     case {List.first(play), List.last(play)} do
-      {l, r} when is_draw({l, r}) -> 3 + rightCost(r)
-      {l, r} when is_leftWon({l, r}) -> 0 + rightCost(r)
-      {_, r} -> 6 + rightCost(r)
+      {l, r} when is_draw({l, r}) -> 3 + cost(r)
+      {l, r} when is_leftWon({l, r}) -> 0 + cost(r)
+      {_, r} -> 6 + cost(r)
     end
   end
 
-  def rightCost(right) do
-    case right do
+  def getRoundResultPartTwo(play) do
+    case {List.first(play), List.last(play)} do
+      {l, r} when is_draw_partTwo(r) -> 3 + cost(l)
+      {l, r} when is_leftWon_partTwo(r) -> 0 + cost_toLoose(l)
+      {l, _} -> 6 + cost_toWin(l)
+    end
+  end
+
+  def cost(choose) do
+    case choose do
       "X" -> 1
+      "A" -> 1
       "Y" -> 2
+      "B" -> 2
       "Z" -> 3
+      "C" -> 3
+    end
+  end
+
+  def cost_toLoose(choose) do
+    case choose do
+      "A" -> 3
+      "B" -> 1
+      "C" -> 2
+    end
+  end
+
+  def cost_toWin(choose) do
+    case choose do
+      "A" -> 2
+      "B" -> 3
+      "C" -> 1
     end
   end
 end
