20d7f5516fd10fa4e1e55d467e906a4dd823ff0e
Kirill Petrov <krll.ptrv@gmail.com>
2022-12-18 07:18:27 +0200

    day 16

--- day16/lib/day16.ex
+++ day16/lib/day16.ex
@@ -1,49 +1,48 @@
 defmodule Day16 do
   # coef: 30 start, move - 1, release - 1
-  def get_result(file) do
+  def get_result(file \\ "input-0.txt") do
     input =
       file
       |> get_input
 
-    path_list = build_path("AA", input, {%{"AA" => true}, ["AA"], []})
+    build_path(input, "AA", [["AA"]])
 
-    path_list
-    # |> elem(2)
-    # |> Enum.map(fn r ->
-    #   r
-    #   |> Enum.map(fn i -> input |> Map.fetch!(i) |> elem(0) end)
-    # end)
+    # path_list = build_path("AA", input, {%{"AA" => true}, ["AA"], []})
   end
 
-  def build_path(start, input, {visited, path, result}) do
-    options = input |> Map.fetch!(start) |> elem(1)
-
-    options
-    |> Enum.filter(fn i -> visited |> Map.has_key?(i) == false end)
-    |> Enum.map(fn o ->
-      updated = path ++ [o]
-
-      new_result = result ++ [updated]
-
-      IO.inspect("res")
-      IO.inspect(new_result)
-
-      # new_result ++
-      build_path(
-        o,
-        input,
-        {visited |> Map.merge(%{o => true}), updated, new_result}
-      )
+  def build_path(input, current, result) do
+    input
+    |> Map.fetch!(current)
+    |> elem(1)
+    |> Enum.reduce(result, fn o, acc ->
+      result
+      |> Enum.reduce(acc, fn r, acc ->
+        acc ++ [r ++ [o]]
+      end)
     end)
-
-    # |> Enum.reduce({path, result}, fn o, {path, result} ->
-    #   build_path(
-    #     o,
-    #     input,
-    #     {visited |> Map.merge(%{o => true}), path ++ [o], result ++ [path ++ [o]]}
-    #   )
-    # end)
   end
+
+  # def build_path(start, input, {visited, path, result}) do
+  #   options = input |> Map.fetch!(start) |> elem(1)
+
+  #   options
+  #   |> Enum.filter(fn i -> visited |> Map.has_key?(i) == false end)
+  #   |> Enum.map(fn o ->
+  #     updated = path ++ [o]
+
+  #     new_result = result ++ [updated]
+
+  #     IO.inspect("res")
+  #     IO.inspect(new_result)
+
+  #     # new_result ++
+  #     build_path(
+  #       o,
+  #       input,
+  #       {visited |> Map.merge(%{o => true}), updated, new_result}
+  #     )
+  #   end)
+  # end
 
   def parse_row(r) do
     # Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
